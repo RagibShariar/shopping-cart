24920bb2d1c0777e8f483b881b3ce12856ea104b
RagibShariar <srragib@gmail.com>
2022-02-15 14:36:04 +0600

    calculation added

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// counting phone quantity and case quantity
 function updatePrice(product, price, isIncreasing){
     const productInput = document.getElementById(product+'-count');
     const productNumber = productInput.value;
@@ -11,10 +12,10 @@ function updatePrice(product, price, isIncreasing){
     // update price
     const productPrice = document.getElementById(product+'-price');
     productPrice.innerText = price * Number(productInput.value);
+
+    // calculate total
+    calculateTotal();
 };
-
-
-
 
 // phone
 document.getElementById('phone-plus').addEventListener('click', function(){
@@ -31,3 +32,23 @@ document.getElementById('case-plus').addEventListener('click', function(){
 document.getElementById('case-minus').addEventListener('click', function(){
     updatePrice('case',59, false);
 });
+
+
+// total price 
+function calculateTotal(){
+    const phoneCount = parseInt(document.getElementById('phone-count').value);
+    const caseCount = parseInt(document.getElementById('case-count').value);
+
+    const phoneTotal = phoneCount * 1219;
+    const caseTotal = caseCount * 59;
+
+    const subTotal = phoneTotal + caseTotal;
+    console.log(subTotal);
+
+    const tax = subTotal / 10;  // 10%
+    const total = subTotal + tax; 
+
+    document.getElementById('subtotal').innerText = subTotal;
+    document.getElementById('tax').innerText = tax;
+    document.getElementById('total').innerText = total;
+};
