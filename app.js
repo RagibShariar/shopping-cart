69ad709ab6d3bb043c56327e7b97f3a1ada9491f
RagibShariar <srragib@gmail.com>
2022-02-15 13:53:56 +0600

    phone and case count added

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
-console.log('I am in a separate file')
+function updatePrice(product, price, isIncreasing){
+    const productInput = document.getElementById(product+'-count');
+    const productNumber = productInput.value;
+    if (isIncreasing) {
+        productInput.value =  Number(productNumber) +1;
+    }
+    else if(productInput.value > 0 ){
+        productInput.value =  Number(productNumber) -1;
+    }
+
+    // update price
+    const productPrice = document.getElementById(product+'-price');
+    productPrice.innerText = price * Number(productInput.value);
+};
+
+
+
+
+// phone
+document.getElementById('phone-plus').addEventListener('click', function(){
+    updatePrice('phone', 1219, true);
+});
+document.getElementById('phone-minus').addEventListener('click', function(){
+    updatePrice('phone', 1219, false);
+});
+
+// case
+document.getElementById('case-plus').addEventListener('click', function(){
+    updatePrice('case', 59, true);
+});
+document.getElementById('case-minus').addEventListener('click', function(){
+    updatePrice('case',59, false);
+});
